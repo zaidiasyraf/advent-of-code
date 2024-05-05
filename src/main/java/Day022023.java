970b36e60f7b72bbf1ba59e2497944197eb7f7c1
unknown <zaidi.asyraf@yahoo.com>
2024-05-05 16:06:34 +0800

    Add day 2 2023

--- src/main/java/Day022023.java
+++ src/main/java/Day022023.java
@@ -6,46 +6,29 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Day022023 {
 
+    private static final int RED_LIMIT = 12;
+    private static final int GREEN_LIMIT = 13;
+    private static final int BLUE_LIMIT = 14;
+
     public static void main(String[] args) {
-        try (BufferedReader reader =  new BufferedReader(new FileReader("src/main/resources/Day022023.txt"))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/Day022023.txt"))) {
             String line = reader.readLine();
-            int redLimit = 12;
-            int greenLimit = 13;
-            int blueLimit = 14;
             int totalAnswer1 = 0;
+            int totalAnswer2 = 0;
             while (line != null) {
                 line = StringUtils.deleteWhitespace(line);
                 String[] splitColon = line.split(":");
                 int currentGame = Integer.parseInt(splitColon[0].replace("Game", ""));
 
                 String[] splitSemiColon = splitColon[1].split(";");
-                int blueCount = 0;
-                int redCount = 0;
-                int greenCount = 0;
-                for (String semiColon : splitSemiColon) {
-                    String[] splitComma = semiColon.split(",");
-                    for (String comma : splitComma) {
-                        if (comma.contains(COLOUR.BLUE.name().toLowerCase(Locale.ROOT))) {
-                            blueCount = blueCount + Integer.parseInt(comma.replace(COLOUR.BLUE.name().toLowerCase(Locale.ROOT), ""));
-                            continue;
-                        } else if (comma.contains(COLOUR.RED.name().toLowerCase(Locale.ROOT))) {
-                            redCount = redCount + Integer.parseInt(comma.replace(COLOUR.RED.name().toLowerCase(Locale.ROOT), ""));
-                            continue;
-                        } else if (comma.contains(COLOUR.GREEN.name().toLowerCase(Locale.ROOT))) {
-                            greenCount = greenCount + Integer.parseInt(comma.replace(COLOUR.GREEN.name().toLowerCase(Locale.ROOT), ""));
-                            continue;
-                        }
-                        throw new IllegalStateException("weird");
-                    }
-                }
-                if (blueCount <= blueLimit && redCount <= redLimit && greenCount <= greenLimit) {
-                    totalAnswer1 = totalAnswer1 + currentGame;
-                }
+                totalAnswer1 = firstQuestionLogic(currentGame, totalAnswer1, splitSemiColon);
+                totalAnswer2 = secondQuestionLogic(totalAnswer2, splitSemiColon);
 
                 // read next line
                 line = reader.readLine();
             }
             System.out.println("Answer 1 : " + totalAnswer1);
+            System.out.println("Answer 2 : " + totalAnswer2);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -55,6 +38,97 @@ public class Day022023 {
         BLUE,
         RED,
         GREEN
+    }
+
+    private static int firstQuestionLogic(int currentGame, int totalAnswer1, String[] splitSemiColon) {
+        try {
+            for (String semiColon : splitSemiColon) {
+                String[] splitComma = semiColon.split(",");
+                for (String comma : splitComma) {
+                    if (comma.contains(COLOUR.BLUE
+                            .name()
+                            .toLowerCase(Locale.ROOT))) {
+                        int ballCount = Integer.parseInt(comma.replace(COLOUR.BLUE
+                                .name()
+                                .toLowerCase(Locale.ROOT), ""));
+                        if (ballCount > BLUE_LIMIT) {
+                            throw new IllegalStateException("Wrong");
+                        }
+                        continue;
+                    } else if (comma.contains(COLOUR.RED
+                            .name()
+                            .toLowerCase(Locale.ROOT))) {
+                        int ballCount = Integer.parseInt(comma.replace(COLOUR.RED
+                                .name()
+                                .toLowerCase(Locale.ROOT), ""));
+                        if (ballCount > RED_LIMIT) {
+                            throw new IllegalStateException("Wrong");
+                        }
+                        continue;
+                    } else if (comma.contains(COLOUR.GREEN
+                            .name()
+                            .toLowerCase(Locale.ROOT))) {
+                        int ballCount = Integer.parseInt(comma.replace(COLOUR.GREEN
+                                .name()
+                                .toLowerCase(Locale.ROOT), ""));
+                        if (ballCount > GREEN_LIMIT) {
+                            throw new IllegalStateException("Wrong");
+                        }
+                        continue;
+                    }
+                    throw new IllegalStateException("weird");
+                }
+            }
+            return totalAnswer1 + currentGame;
+        } catch (IllegalStateException e) {
+            System.out.println(currentGame + " is not possible");
+        }
+        return totalAnswer1;
+    }
+
+    private static int secondQuestionLogic(int totalAnswer2, String[] splitSemiColon) {
+        int maxRed = 0;
+        int maxGreen = 0;
+        int maxBlue = 0;
+        for (String semiColon : splitSemiColon) {
+            String[] splitComma = semiColon.split(",");
+            for (String comma : splitComma) {
+                if (comma.contains(COLOUR.BLUE
+                        .name()
+                        .toLowerCase(Locale.ROOT))) {
+                    int ballCount = Integer.parseInt(comma.replace(COLOUR.BLUE
+                            .name()
+                            .toLowerCase(Locale.ROOT), ""));
+                    if (ballCount > maxBlue) {
+                        maxBlue = ballCount;
+                    }
+                    continue;
+                } else if (comma.contains(COLOUR.RED
+                        .name()
+                        .toLowerCase(Locale.ROOT))) {
+                    int ballCount = Integer.parseInt(comma.replace(COLOUR.RED
+                            .name()
+                            .toLowerCase(Locale.ROOT), ""));
+                    if (ballCount > maxRed) {
+                        maxRed = ballCount;
+                    }
+                    continue;
+                } else if (comma.contains(COLOUR.GREEN
+                        .name()
+                        .toLowerCase(Locale.ROOT))) {
+                    int ballCount = Integer.parseInt(comma.replace(COLOUR.GREEN
+                            .name()
+                            .toLowerCase(Locale.ROOT), ""));
+                    if (ballCount > maxGreen) {
+                        maxGreen = ballCount;
+                    }
+                    continue;
+                }
+                throw new IllegalStateException("weird");
+            }
+        }
+        int total = maxRed * maxGreen * maxBlue;
+        return totalAnswer2 + total;
     }
 
 }
